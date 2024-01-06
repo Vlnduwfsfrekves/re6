47a22aac6152c6becfc49367126e5990fc089be4
Vlnduwfsfrekves <112093090+Vlnduwfsfrekves@users.noreply.github.com>
2024-01-06 10:10:24 +0200

    -d

---
+++ src/components/Event.jsx
@@ -0,0 +1,2 @@
+import React from "react";
+export const Event=({name, start, end, location, speaker})=>{}
---
+++ src/components/PageBoard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Event from './Event'
+import { FaReact } from 'react-icons/fa';
+const PageBoard=(Event)=>{
+    <h2></h2>
+    <p><FaReact/></p>
+    <p></p>
+    <p></p>
+    <p></p>
+}
